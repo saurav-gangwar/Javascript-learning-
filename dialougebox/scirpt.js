dbea48064d48fd6760370c35220d24701b58cfaf
Saurav Gangwar <sauravgangwar30@gmail.com>
2026-02-28 17:29:08 +0530

    dialouge box

--- dialougebox/scirpt.js
+++ dialougebox/scirpt.js
@@ -1,4 +1,7 @@
 // const alertResult = alert("Are you sure?")
+// -- alert box show only pop up message . on wondow object 
 // const isConfirmed = confirm("Would you like to proceed?")
+// -- confirm the message ture or false 
 const userInput = prompt("Please enter your name.........................")
 console.log(userInput)
+// -- It takes input from the user .
