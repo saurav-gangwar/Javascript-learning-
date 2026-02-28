1bd3e26d32feda8e417765b81f5db1faed2788b1
Saurav Gangwar <sauravgangwar30@gmail.com>
2026-02-28 17:21:32 +0530

    debugging

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+console.log(firstName);
 var firstName = 'Akash'
 let lastName = 'Singh'
 let age = 15
 const yearOfBirth = 1999 
 
-let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
-let num = 4 ;
-let num2 = 5;
-console.log(num +num2); 
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
